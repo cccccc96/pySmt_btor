6b853d1186efa57bcb9acf56eaed7a316a7df356
mufanx <35735090+mufanx@users.noreply.github.com>
2022-03-17 19:02:58 +0800

    It works

--- preexp.py
+++ preexp.py
@@ -14,7 +14,7 @@ def at_time(v, t):
 class TransitionSystem(object):
     """Trivial representation of a Transition System."""
 
-    def __init__(self, variables, init, trans, update: dict):
+    def __init__(self, variables, init, trans, update):
         self.variables = variables
         self.init = init
         self.trans = trans
@@ -32,7 +32,7 @@ class KInduction:
         subs_i = {}
         for v in self.system.variables:
             subs_i[v] = at_time(v, i)
-            subs_i[next_var(v)] = at_time(v, i+1)
+            subs_i[next_var(v)] = at_time(v, i + 1)
         return subs_i
 
     def get_unrolling(self, k):
@@ -41,7 +41,7 @@ class KInduction:
         E.g. T(0,1) & T(1,2) & ... & T(k-1,k)
         """
         res = []
-        for i in range(k+1):
+        for i in range(k + 1):
             subs_i = self.get_subs(i)
             res.append(self.system.trans.substitute(subs_i))
         return And(res)
@@ -51,9 +51,9 @@ class KInduction:
         each time encodes a different state
         """
         res = []
-        for i in range(k+1):
+        for i in range(k + 1):
             subs_i = self.get_subs(i)
-            for j in range(i+1, k+1):
+            for j in range(i + 1, k + 1):
                 state = []
                 subs_j = self.get_subs(j)
                 for v in self.system.variables:
@@ -91,21 +91,59 @@ class KInduction:
         print("Checking property %s..." % self.prop)
         for b in range(100):
             f = self.get_bmc(b)
-            print("   [BMC]    Checking bound %d..." % (b+1))
+            print("   [BMC]    Checking bound %d..." % (b + 1))
             if is_sat(f):
-                print("--> Bug found at step %d" % (b+1))
+                print("--> Bug found at step %d" % (b + 1))
                 return
 
             f = self.get_k_induction(b)
-            print("   [K-IND]  Checking bound %d..." % (b+1))
+            print("   [K-IND]  Checking bound %d..." % (b + 1))
             if is_unsat(f):
                 print("--> The system is safe!")
                 return
             else:
-                print("--> Strengthen the property.")
+                print("   [STRONG] Strengthen the property.")
                 self.strength = self.strength.substitute(self.system.update)
                 self.prop = And(self.strength, self.prop)
 
 
+def counter(bit_count):
+    """Counter example with n bits and reset signal."""
+
+    # Example Counter System (SMV-like syntax)
+    #
+    # VAR bits: word[bit_count];
+    #     reset: boolean;
+    #
+    # INIT: bits = 0 & reset = FALSE;
+    #
+    # TRANS: next(bits) = bits + 1
+    # TRANS: next(bits = 0) <-> next(reset)
+
+    from pysmt.typing import BVType
+    bits = Symbol("bits", BVType(bit_count))
+    nbits = next_var(bits)
+    reset = Symbol("r", BOOL)
+    variables = [bits, reset]
+
+    init = bits.Equals(0) & Not(reset)
+
+    trans = nbits.Equals(bits + 1) & (nbits.Equals(0)).Iff(bits.Equals(10) | reset)
+
+    update = dict()
+    update[bits] = Ite(bits.Equals(10) | reset, BV(1, 8), bits + 1)
+
+    # A true invariant property: (reset -> bits = 0)
+    true_prop = BVULT(bits, BV(20, 8))
+
+    # A false invariant property: (bits != 2**bit_count-1)
+    false_prop = BVULT(bits, BV(8, 8))
+
+    return TransitionSystem(variables, init, trans, update), [true_prop, false_prop]
+
+
 if __name__ == '__main__':
-    pass
+    ts, props = counter(8)
+    ind = KInduction(ts, props[0])
+    ind.check_property()
+
