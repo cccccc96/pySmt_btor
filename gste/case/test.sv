fb83942ef4fdfd048e74fdbd27d01505cfa05bd8
caizhenghai <47801930+caizhenghai@users.noreply.github.com>
2022-08-15 08:57:41 +0800

    unroll not post

--- gste/case/test.sv
+++ gste/case/test.sv
@@ -1,13 +1,5 @@
-// Ring buffer FIFO.
-// head points to the insertion point unless the buffer is full.
-// tail points to the first element in the queue unless the buffer is empty.
-// A push on a full buffer is a NOOP.
-// A pop from an empty buffer is a NOOP.
-// If both push and pop are asserted at the same clock cycle, only the push
-// operation is performed.
-// dataOut gives the first element of the queue unless the buffer is empty,
-// in which case its value is arbitrary.
-module rbFIFO(clock,dataIn,push,pop,dataOut,full,empty,test);
+
+module rbFIFO(clock,dataIn,push,pop,dataOut,full,empty);
     parameter	    MSBD = 3;
     parameter	    LAST = 15;
     parameter	    MSBA = 3;
@@ -18,12 +10,12 @@ module rbFIFO(clock,dataIn,push,pop,dataOut,full,empty,test);
     output [MSBD:0] dataOut;
     output	    full;
     output	    empty;
-    output [MSBA:0]     test;
 
     reg [MSBD:0]    mem[0:LAST];
     reg [MSBA:0]    head;
     reg [MSBA:0]    tail;
     reg		    empty;
+    reg         full;
     integer	    i;
 
     initial begin
@@ -35,12 +27,20 @@ module rbFIFO(clock,dataIn,push,pop,dataOut,full,empty,test);
     end // initial begin
 
     always @ (posedge clock) begin
-	    tail = tail+1;
+	if (push & ~full) begin
+	    mem[head] = dataIn;
+	    head = head + 1;
+	    empty = 0;
+	end // if (push & ~full)
+	else if (pop & ~empty) begin
+	    tail = tail + 1;
+	    if (tail == head)
+		empty = 1;
+	end // if (pop & ~empty)
     end // always @ (posedge clock)
 
+    assign dataOut = mem[tail];
 
-    assign test = tail+2;
-
-
+    assign full = (tail == head) & ~empty;
 
 endmodule // rbFIFO
